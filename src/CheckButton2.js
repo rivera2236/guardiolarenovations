ec09897af8e302fad3fc8dd1080c1a3449514904
jorge rivera <rivera2236@gmail.com>
2020-10-20 16:58:36 -0400

    deleted all the http// from the project

--- src/CheckButton2.js
+++ src/CheckButton2.js
@@ -56,7 +56,7 @@ class CheckButton extends Component {
             fill={this.fill()}
             height="24" viewBox="0 0 24 24"
             width="24"
-            xmlns="http://www.w3.org/2000/svg">
+            xmlns="">
 
                 <radialGradient
             id="shadow"
